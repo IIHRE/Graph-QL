0863a95b807f55b65a86a7fbb7a1b2bff0f41f22
sandun <sandun.wed@gmail.com>
2020-04-11 19:10:03 +0530

    add schema

--- GraphQl Project/server/schema/schema.js
+++ GraphQl Project/server/schema/schema.js
@@ -1,10 +1,19 @@
 const graphql = require('graphql');
+const_=require('lodash');
 
-const {GraphQLObjectType, GraphQLString} = graphql;
+const {GraphQLObjectType, GraphQLString, GraphQLScema} = graphql;
+
+//dummy data
+
+var books = [
+    {name: 'Name of the Wind',genre:"Fantasy", id: '1'},
+     {name: 'Name of the Empire',genre:"Commedy", id: '2'},
+      {name: 'Name of the Wind des',genre:"Fantasy", id: '3'},
+];
 
 const BookType = new GraphQLObjectType({
 name: 'Book',
-field: ()=>({
+fields:()=>({
     id: {type: GraphQLString},
     name: {type: GraphQLString},
     genre: {type: GraphQLString}
@@ -15,11 +24,22 @@ field: ()=>({
 
 const RootQuery = new GraphQLObjectType({
 name: 'RootQueryType',
-field: {
-    book: {
-    type: BookType,
-    args: {id: {type:GraphQLString}}
+    fields: {
+        book: {
+        type: BookType,
+        args: {id: {type:GraphQLString}},
+            resolve(parent,args){
+            //code to get data from db
+            return _.find(books, {id:args.id});
+            }
+        }
     }
-    }
-    
- })
+
+ });
+
+
+
+ module.exports = new GraphQLScema({
+    query: RootQuery
+
+ });
